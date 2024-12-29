1c64fc382da4b0139b3386a8db978e64a2a51a1b
urodstvo <dead.kanji@mail.ru>
2024-12-29 19:21:24 +0300

    build: init

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import path from 'path';
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+})
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+
+/** @type {import('prettier').Config} */
+const config = {
+    semi: true,
+    tabWidth: 4,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    printWidth: 120,
+}
+  
+  export default config
